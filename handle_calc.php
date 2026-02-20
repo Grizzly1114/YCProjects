abafa6a336e34ed04631408e25db3d0e310673e3
Anthony Battista <onemilesixfive1980@gmail.com>
2026-02-20 15:09:45 -0700

    Add files via upload

--- handle_calc.php
+++ handle_calc.php
@@ -1,4 +1,51 @@
-git rm handle_calc.php
-git commit -m "Remove handle_calc.php"
-git push origin main
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Product Cost Calculator</title>
+    <style type="text/css">
+        .number { font-weight: bold; }
+    </style>
+</head>
+<body>
+
+<?php
+// Script 4.3 - handle_calc2.php
+/* This script takes values from calculator.html and performs total cost and monthly payment calculations. */
+
+// Get values from POST
+$price = $_POST['price'];
+$quantity = $_POST['quantity'];
+$discount = $_POST['discount'];
+$tax = $_POST['tax'];
+$shipping = $_POST['shipping'];
+$payments = $_POST['payments'];
+
+// Calculate the total
+$total = $price * $quantity;
+$total = $total + $shipping;
+$total = $total - $discount;
+
+// Determine the tax rate
+$taxrate = $tax / 100;
+$taxrate = $taxrate + 1;
+
+// Factor in tax rate
+$total = $total * $taxrate;
+
+// Calulate the monthly payment
+$monthly = $total / $payments;
+
+
+// Print out the results:
+print "<p>You have selected to purchase:<br>
+<span class=\"number\">$quantity</span> widget(s) at <span class=\"number\">$price</span> price each plus a<br>
+<span class=\"number\">$shipping</span> shipping cost and a<br>
+<span class=\"number\">$tax</span>% tax rate.<br>
+After your $<span class=\"number\">$discount</span> discount, the total cost is $<span class=\"number\">$total</span>.<br>
+Divided over <span class=\"number\">$payments</span> monthly payments, that would be $<span class=\"number\">$monthly</span> each.</p>";
+
+?>
+</body>
+</html>
 
