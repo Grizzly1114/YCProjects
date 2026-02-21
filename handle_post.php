e676612a07e489ac3f9c2e841b62a9d11474bc92
Anthony Battista <onemilesixfive1980@gmail.com>
2026-02-20 18:02:34 -0700

    Delete incorrect handle_post.php file

--- handle_post.php
+++ handle_post.php
@@ -1,37 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Forum Posting Results</title>
-</head>
-<body>
+# Remove the file from git tracking
+git rm handle_post.php
 
-<?php
-// Script 5.2 - handle_post.php
-// This script receives values from posting.html
+# Commit the deletion
+git commit -m "Delete wrong handle_post.php file"
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+# Push the deletion
+git push origin main
 
-    // Get the values from the form
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $posting = $_POST['posting'];
-
-    // Create a full name variable
-    $name = $first_name . " " . $last_name;
-
-    // Display a message
-    echo "<div>";
-    echo "<p>Thank you, $name, for your posting:</p>";
-    echo "<p>$posting</p>";
-    echo "<p>We will contact you at: $email</p>";
-    echo "</div>";
-
-} else {
-    echo "<p>This page should not be accessed directly.</p>";
-}
-?>
-
-</body>
-</html>
