7fab827a1e3da4ca6485e434f659dd2202fb6c9c
Anthony Battista <onemilesixfive1980@gmail.com>
2026-02-20 18:03:06 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -1,9 +1,30 @@
-# Remove the file from git tracking
-git rm handle_post.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Forum Posting Results</title>
+</head>
+<body>
 
-# Commit the deletion
-git commit -m "Delete wrong handle_post.php file"
+<?php
+// Script 5.2 - handle_post.php
+// This script receives values from posting.html
 
-# Push the deletion
-git push origin main
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    // Get the values from the form
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $posting = $_POST['posting'];
+
+    // Create a full name variable
+    $name = $first_name . " " . $last_name;
+
+    // print a message
+	print "<div>Thank you, $name, for your posting:
+	<p>$posting</p></div>";
+	
+?>
+</body>
+</html>
