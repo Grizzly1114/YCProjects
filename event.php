982eebc9f2482d534727731c80fd50942912d175
Anthony Battista <onemilesixfive1980@gmail.com>
2026-03-01 19:10:00 -0700

    Add files via upload

---
+++ event.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Add an Event</title>
+</head>
+<body>
+<?php
+// Script 7.9 - event.php
+/* This script handles the event form. */
+
+// address error management if you want
+
+// Print the text:
+print "<p>You want to add an event called 
+<b>{$_POST['name']}</b> which takes place on:<br>";
+
+//Print each week day:
+if (isset($_POST['days']) AND is_array($_POST['days'])) {
+
+    foreach ($_POST['days'] as $day) {
+        print "$day<br>\n";
+    }
+
+} else {
+
+    print 'Please select at least one weekday for this event!';
+
+}
+
+// Complete the paragraph
+print '</p>';
+?>
+</body>
+</html>
