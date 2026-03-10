bc6c720acbea0abd072f85276034259d1486fc31
Anthony Battista <onemilesixfive1980@gmail.com>
2026-03-10 13:56:09 -0700

    Add files via upload

--- add_quote.php
+++ add_quote.php
@@ -1,43 +1,49 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Add A Quotation</title>
+    <meta charset="utf-8">
+    <title>Add A Quotation</title>
 </head>
 <body>
-<?php //Script 11.1 - add_quote.php
+<?php 
+// Script 11.1 - add_quote.php
 
 // Identify the file to use
-$file = '../quotes.txt';;
+$file = '../quotes.txt';
 
 // Check for a form submission
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
 
-	if (!empty($_POST['quote']) && ($_POST['quote'] != 'Enter your quotation here.')) { 
-	
-		if (is_writable($file)) { 
-			
-			file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND); 
+    // Make sure the user entered something and it's not the placeholder text
+    if (!empty($_POST['quote']) && ($_POST['quote'] != 'Enter your quotation here.')) { 
 
-			// Print message
-			print '<p>Your quotation has been stored.</p>';
-		
-		} else { 
-			print '<p style="color: red;">Your quotation could not be stored due to a system error.</p>';
-		}
-		
-	} else { 
-		print '<p style="color: red;">Please enter a quotation!</p>';
-	}
-	
+        // Confirm that the file is writable
+        if (is_writable($file)) { 
+
+            // Write the data to the file, appending a newline and locking the file
+            file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND | LOCK_EX); 
+
+            // Print message
+            print '<p>Your quotation has been stored.</p>';
+
+        } else { 
+            // Could not open the file
+            print '<p style="color: red;">Your quotation could not be stored due to a system error.</p>';
+        }
+
+    } else { 
+        // Failed to enter a quotation
+        print '<p style="color: red;">Please enter a quotation!</p>';
+    }
+
 } 
 
-// Leave PHP and display the form:
+// Leave PHP and display the form
 ?>
 
 <form action="add_quote.php" method="post">
-	<textarea name="quote" rows="5" cols="30">Enter your quotation here.</textarea><br>
-	<input type="submit" name="submit" value="Add This Quote!">
+    <textarea name="quote" rows="5" cols="30" placeholder="Enter your quotation here."></textarea><br>
+    <input type="submit" name="submit" value="Add This Quote!">
 </form>
 
 </body>
